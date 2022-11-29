7e001d33476e06fd310a3e3b86fb0d1f3970e0a9
Simon Pucheu <104694344+6moon9@users.noreply.github.com>
2022-11-29 19:42:53 +0100

    Update

--- src/Bull/Bull.ino
+++ src/Bull/Bull.ino
@@ -2,6 +2,7 @@
 #include <Keybull.h>
 #include <Joystick.h>
 #include <Led.h>
+#include <Report.h>
 
 #define loopTime 80
 
@@ -9,12 +10,13 @@ SoftwareSerial Serial1(2, 3);
 
 Bluetooth bluetooth(&Serial1);
 Keybull keybull(4);
-Joystick leftJoystick(A0, A1, A2);
-Joystick rightJoystick(A3, A4, A5);
+Joystick leftJoystick(A0, A1, 0);
+Joystick rightJoystick(A2, A3, 0, true);
 Button switcher(A6);
 Led led1(11);
 Led led2(12);
 Led builtin(LED_BUILTIN);
+Report report(&Serial, true, 5 * (1000 / loopTime));
 
 void setup ()
 {
@@ -28,6 +30,23 @@ void setup ()
 
 void loop ()
 {
+  if (bluetooth.receive())
+  {
+    if (bluetooth.lastError == DeserializationError::Ok)
+    {
+      report.ok++;
+    }
+    else
+    {
+      report.inv++;
+      bluetooth.empty();
+    }
+  }
+  else
+  {
+    report.ntr++;
+  }
+  report.print();
   // Fetch data to json and send it //
   {
     bluetooth.json["switcher"] = switcher.getValue();
@@ -45,9 +64,9 @@ void loop ()
       }
     }
     bluetooth.send();
+    //serializeJsonPretty(bluetooth.json, Serial);
+    //Serial.println();
+    //Serial.println();
   }
-  //led1.on();
-  //led2.off();
-  //led3.on();
   delay(loopTime);
 }
